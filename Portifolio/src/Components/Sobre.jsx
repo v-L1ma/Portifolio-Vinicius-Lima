457d7ab527c0008656843223f858e8450aaa3063
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2024-12-29 23:06:40 -0300

    atualizando o texto

--- Portifolio/src/Components/Sobre.jsx
+++ Portifolio/src/Components/Sobre.jsx
@@ -5,11 +5,11 @@ function Sobre(){
 
         <div className="mt-20 pb-10 lg:mt-80">
             <p className="lg:w-3/5 m-auto text-3xl">
-              Atualmente cursando o 3º semestre da Faculdade de 
+              Atualmente, curso o 4º semestre da Faculdade de 
               <strong className="text-red-900"> Analise e desenvolvimento de sistemas</strong> na
-              <strong className="text-red-900"> FATEC - Rubens Lara</strong> estou buscando uma oportunidade
+              <strong className="text-red-900"> FATEC - Rubens Lara.</strong> Busco uma oportunidade
               de entrar no mercado de trabalho na parte de
-              <strong className="text-red-900"> Front-End</strong>. Estou estudando tecnologias
+              <strong className="text-red-900"> Desenvolvimento Front-End</strong>. Estou estudando tecnologias como:
               <strong className="text-red-900"> React, JavaScript, TailWind, Axios, NodeJS, MongoDB, MySQL ,HTML & CSS
               </strong>
               , tenho mais afinidade com o Front-End pois sou apaixonado pela parte
